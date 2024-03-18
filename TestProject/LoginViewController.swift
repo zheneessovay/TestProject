748f369693cf394453b7dbe0dfaff885cd4dffb1
Yerkezhan Zhenissova <zheneessovay@gmail.com>
2024-03-18 20:50:17 +0500

    login page added

---
+++ TestProject/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  TestProject
+//
+//  Created by Yerkezhan Zheneessova on 18.03.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("login page")
+    }
+
+}
