3b016a48ea315b93ff9051054ac10e0bb3beb24e
Yerkezhan Zhenissova <zheneessovay@gmail.com>
2024-03-18 20:30:31 +0500

    second hello world added

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        print("hello world")
+        print("hello world2")
+        
     }
-
 
 }
 
