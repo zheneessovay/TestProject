f2db5e4feadb194ce27153e95aea82e6876c33a0
Yerkezhan Zhenissova <zheneessovay@gmail.com>
2024-03-18 20:16:54 +0500

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Yerkezhan Zheneessova on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
